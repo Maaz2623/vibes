81456a97abbce6d966bcf6e8c87ca37a5ca904a8
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-06-27 20:33:00 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/page.tsx 	modified:   src/inngest/functions.ts 	modified:   src/trpc/routers/_app.ts

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
 import { inngest } from "./client";
+import { createAgent, gemini, } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert nextjs developer. You write readable, maintanable code. You write simple Next.js & React snippets.",
+      model: gemini({
+        model: "gemini-2.0-flash-lite",
+        apiKey: process.env.GEMINI_API_KEY!
+      })
+    })
+
+    const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+    console.log(output);
+
+    return {
+      message: output
+    }
   },
 );
